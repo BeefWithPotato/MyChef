a733af2fe1b6e446b153f133814ef90fd4a71da2
Haosheng Zhang <haoshengg.zhang@mail.utoronto.ca>
2020-11-01 21:20:27 -0500

    register button

--- app/src/main/java/com/example/mychef/RegisterActivity.java
+++ app/src/main/java/com/example/mychef/RegisterActivity.java
@@ -2,13 +2,17 @@ package com.example.mychef;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText phone;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,14 @@ public class RegisterActivity extends AppCompatActivity {
         Drawable drawable1 =  getResources().getDrawable(R.drawable.phone);
         drawable1.setBounds(0, 0, 70, 70);
         phone.setCompoundDrawables(drawable1,null, null, null);
+
+        register = (Button) findViewById(R.id.btn_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

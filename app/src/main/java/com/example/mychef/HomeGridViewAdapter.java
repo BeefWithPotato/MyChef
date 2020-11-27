0c6a1bbaf0be066af0b273cf29e7d02cdc201218
shiwenb2 <wenbo.shi@mail.utoronto.ca>
2020-11-26 20:54:54 -0500

    Add user icon and user name for Home explore page

--- app/src/main/java/com/example/mychef/HomeGridViewAdapter.java
+++ app/src/main/java/com/example/mychef/HomeGridViewAdapter.java
@@ -10,6 +10,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -17,6 +22,9 @@ public class HomeGridViewAdapter extends BaseAdapter{
 
     private Context mContext;
     private LayoutInflater mLayoutInflater;
+    private User userInfo;
+    private Recipe recipe;
+    private DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
     ArrayList<Recipe> recipes;
     public HomeGridViewAdapter(Context context, ArrayList<Recipe> recipes){
         this.mContext = context;
@@ -40,8 +48,8 @@ public class HomeGridViewAdapter extends BaseAdapter{
     }
 
     static class ViewHolder{
-        public ImageView imageView;
-        public TextView textView;
+        public ImageView imageView, profile_icon;
+        public TextView textView, userName;
     }
 
     @Override
@@ -52,12 +60,37 @@ public class HomeGridViewAdapter extends BaseAdapter{
             holder = new ViewHolder();
             holder.imageView = (ImageView) convertView.findViewById(R.id.Grid_img);
             holder.textView = (TextView)convertView.findViewById(R.id.Title);
+            holder.profile_icon = (ImageView) convertView.findViewById(R.id.profile_image);
+            holder.userName = (TextView) convertView.findViewById(R.id.username);
             convertView.setTag(holder);
         } else{
             holder = (ViewHolder) convertView.getTag();
         }
         holder.textView.setText(recipes.get(position).getRecipeName());
         Glide.with(holder.imageView.getContext()).load(recipes.get(position).getCoverImage()).into(holder.imageView);
+
+
+        ViewHolder finalHolder = holder;
+        ref.child("User").child(recipes.get(position).getAuthorUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userInfo = dataSnapshot.getValue(User.class);
+                //get username
+                finalHolder.userName.setText(userInfo.getUsername());
+
+                //if user has set an icon, then get and set it
+                if(userInfo.getUserIcon() != null){
+                    Glide.with(finalHolder.profile_icon.getContext()).load(userInfo.getUserIcon()).into(finalHolder.profile_icon);
+                }
+                //if not use default
+                else{
+                    finalHolder.profile_icon.setImageResource(R.drawable.usericon);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
         return convertView;
     }
 }
